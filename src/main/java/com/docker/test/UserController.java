aecbc0079c20f9ad622fbd72653ed54489c77165
chetukarankal <ckarankal29@gmail.com>
2025-06-16 19:31:00 +0530

    Initial commit

---
+++ src/main/java/com/docker/test/UserController.java
@@ -0,0 +1,25 @@
+package com.docker.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+}
